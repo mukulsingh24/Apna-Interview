3a426f4ba6e8cb4ff064d9bffbd617bc3a1fa158
Mukul Singh <rmks1004@gmail.com>
2026-07-14 23:18:38 +0530

    model changed testing

--- server/src/services/gemini.service.ts
+++ server/src/services/gemini.service.ts
@@ -6,7 +6,7 @@ export const AnalyzeResume = async (resumeText: string) => {
     apiKey: process.env.GEMINI_API_KEY!,
   });
   const response = await ai.models.generateContent({
-    model: "gemini-3.5-flash",
+    model: "gemini-3.5-flash-lite",
     contents: `
 You are an ATS Resume Analyzer.
 
